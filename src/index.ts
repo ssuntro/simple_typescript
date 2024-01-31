c0cb62a2a01c00db8999909f7df19c722898a7f5
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-01-31 17:53:30 +0700

    setup tsconfig

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,20 @@
 // ts build on top js - static typing, code completioon, refactoring, shorthand notations
 // static type - know type in compile time - C++,c+, java
 // dynamic type = python, js, ruby. determine type at runtime and can be change. heard to utilise the mem and avoid misbehave and predict or optimise processing time.
+
 // dynamic type.cons or ts pan point try to fix - too big edge cases to catch this bug. bug rate tends to higher than static type lang
 // ts = js + type checking
 // js = interpreter, ts = compiler
 // web == various runtime env.
+
 // ts.cons - browser undestand only js. so it need transpilation step to do(+1 step) + need discipline 
 // js = simple projec, mediumt to large proje - ts
+
+
 // ts.cons can catch a log mistake in compile time
-console.log('ddd');
-var age = 20;
+console.log('ddd')
+let age: number = 20
 // age = 's'
+
+// es5 is spec that browser will accept. JS is implementation
+// ES2016 - depends on where you plan to run the JS
